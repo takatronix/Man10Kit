5f502b9d7adcbdb19b9bddb36d2ca5b5441443e6
Takashi Ohtsuka <takatronix@gmail.com>
2017-03-06 13:02:05 +0900

    Kit保存処理

--- src/main/java/red/man10/Man10Kit.java
+++ src/main/java/red/man10/Man10Kit.java
@@ -1,17 +1,131 @@
 package red.man10;
 
+import org.bukkit.Bukkit;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.util.io.BukkitObjectInputStream;
+import org.bukkit.util.io.BukkitObjectOutputStream;
+import org.yaml.snakeyaml.external.biz.base64Coder.Base64Coder;
+
+import javax.security.auth.login.Configuration;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
 
 public final class Man10Kit extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
 
+        // Plugin startup logic
+      //  getServer().getPluginManager().registerEvents (this,this);
+        //
+        getCommand("mkit").setExecutor(new Man10KitCommand(this));
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
     }
+
+    //      キットを保存する
+    public boolean save(Player p, String kitName){
+
+        String kitString = InventoryToBase64.toBase64(p.getInventory());
+
+        String fileName = kitName;
+        File userdata = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "Kits");
+        File f = new File(userdata, File.separator + fileName + ".yml");
+        FileConfiguration data = YamlConfiguration.loadConfiguration(f);
+
+        //When the player file is created for the first time...
+        if (!f.exists()) {
+            try {
+
+                data.set("creator", p.getName());
+                data.set("inventory",kitString);
+
+                data.save(f);
+            } catch (IOException exception) {
+                p.sendMessage("キットの保存に失敗した");
+                exception.printStackTrace();
+                return false;
+            }
+        }
+
+        p.sendMessage("キットを保存した:"+kitName);
+
+        return true;
+    }
+    //      キットを読み込む
+    public boolean load(Player p, String kitName){
+        String fileName = kitName;
+        File userdata = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "Kits");
+        File f = new File(userdata, File.separator + fileName + ".yml");
+        FileConfiguration data = YamlConfiguration.loadConfiguration(f);
+        if (!f.exists()) {
+            p.sendMessage("キットは存在しない:"+kitName);
+            return false;
+        }
+
+        String s = data.getString("inventory");
+    //    p.sendMessage(s);
+        try{
+             Inventory inv = InventoryToBase64.fromBase64(s);
+
+        }
+        catch (IOException e) {
+            p.sendMessage(e.getLocalizedMessage());
+            return false;
+        }
+        p.sendMessage("キットを読み込んだ:"+kitName);
+
+        return true;
+    }
+    boolean saveUserKit(Player p, String kitName){
+
+        String kitString = InventoryToBase64.toBase64(p.getInventory());
+
+
+        String fileName = kitName;
+        File userdata = new File(Bukkit.getServer().getPluginManager().getPlugin("Man10Kit").getDataFolder(), File.separator + "PlayerData");
+        File f = new File(userdata, File.separator + fileName + ".yml");
+        FileConfiguration data = YamlConfiguration.loadConfiguration(f);
+
+        //When the player file is created for the first time...
+        if (!f.exists()) {
+            try {
+
+                data.set("creator", p.getName());
+                data.set("inventory",kitString);
+
+                data.save(f);
+            } catch (IOException exception) {
+                serverMessage("キットの保存に失敗した");
+                exception.printStackTrace();
+                return false;
+            }
+        }
+
+        serverMessage("キットの保存に成功した");
+        return true;
+    }
+
+
+    //     サーバーメッセージ
+    void serverMessage(String text){
+        //command("say "+text);
+        Bukkit.getServer().broadcastMessage(text);
+    }
+
+
+
+
 }
--- src/main/java/red/man10/Man10KitCommand.java
+++ src/main/java/red/man10/Man10KitCommand.java
@@ -10,17 +10,53 @@ import org.bukkit.plugin.java.JavaPlugin;
  * Created by takatronix on 2017/03/06.
  */
 public class Man10KitCommand implements CommandExecutor {
-    JavaPlugin plugin;
-    public FightClubMan10KitCommandCommand(JavaPlugin plugin) {
+    private Man10Kit plugin;
+
+    public Man10KitCommand(Man10Kit plugin) {
         this.plugin = plugin;
     }
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
         Player p = (Player) sender;
 
+        //      引数がない場合
+        if (args.length < 1) {
+            //showHelp(p);
+            return true;
+        }
 
+        if (args[0].equalsIgnoreCase("help")) {
+            // showHelp(p);
+            return true;
+        }
 
+        ////////////////////////////////////
+        //          保存
+        ////////////////////////////////////
+        if (args[0].equalsIgnoreCase("save")) {
 
+            if (args.length < 1) {
+                p.sendMessage("/mkit save [KitName]");
+                return false;
+            }
+
+            plugin.save(p,args[1]);
+            return true;
+        }
+
+        ////////////////////////////////////
+        //          Load
+        ////////////////////////////////////
+        if (args[0].equalsIgnoreCase("load")) {
+
+            if (args.length < 1) {
+                p.sendMessage("/mkit load [KitName]");
+                return false;
+            }
+
+            plugin.load(p,args[1]);
+            return true;
+        }
         return false;
     }
 
