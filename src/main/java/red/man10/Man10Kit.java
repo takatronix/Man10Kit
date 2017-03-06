f5bb8a0e24d960704441ebe6ceb2311065df8f20
Takashi Ohtsuka <takatronix@gmail.com>
2017-03-06 13:42:38 +0900

    Kit保存処理

--- src/main/java/red/man10/Man10Kit.java
+++ src/main/java/red/man10/Man10Kit.java
@@ -19,6 +19,7 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public final class Man10Kit extends JavaPlugin {
 
@@ -47,13 +48,13 @@ public final class Man10Kit extends JavaPlugin {
         File f = new File(userdata, File.separator + fileName + ".yml");
         FileConfiguration data = YamlConfiguration.loadConfiguration(f);
 
-        //When the player file is created for the first time...
+
         if (!f.exists()) {
             try {
 
                 data.set("creator", p.getName());
-                data.set("inventory",kitString);
-
+                data.set("inventory",p.getInventory().getContents());
+                data.set("armor",p.getInventory().getArmorContents());
                 data.save(f);
             } catch (IOException exception) {
                 p.sendMessage("キットの保存に失敗した");
@@ -62,7 +63,7 @@ public final class Man10Kit extends JavaPlugin {
             }
         }
 
-        p.sendMessage("キットを保存した:"+kitName);
+        serverMessage(""+p.getName()+ ":キットを保存した:"+kitName);
 
         return true;
     }
@@ -77,17 +78,33 @@ public final class Man10Kit extends JavaPlugin {
             return false;
         }
 
-        String s = data.getString("inventory");
-        p.sendMessage(s);
-        try{
-             Inventory inv = fromBase64(s);
 
+        Object a = data.get("inventory");
+        Object b = data.get("armor");
+
+        if(a == null || b == null){
+            p.sendMessage("No saved inventory to load");
+            return true;
         }
-        catch (IOException e) {
-            p.sendMessage(e.getLocalizedMessage());
-            return false;
+        ItemStack[] inventory = null;
+        ItemStack[] armor = null;
+        if (a instanceof ItemStack[]){
+            inventory = (ItemStack[]) a;
+        } else if (a instanceof List){
+            List lista = (List) a;
+            inventory = (ItemStack[]) lista.toArray(new ItemStack[0]);
         }
-        p.sendMessage("キットを読み込んだ:"+kitName);
+        if (b instanceof ItemStack[]){
+            armor = (ItemStack[]) b;
+        } else if (b instanceof List){
+            List listb = (List) b;
+            armor = (ItemStack[]) listb.toArray(new ItemStack[0]);
+        }
+        p.getInventory().clear();
+        p.getInventory().setContents(inventory);
+        p.getInventory().setArmorContents(armor);
+
+
 
         return true;
     }
