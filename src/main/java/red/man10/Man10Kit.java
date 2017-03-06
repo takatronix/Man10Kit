b4e63977afe2512c4d5e80d27f9704bdb50197b7
Takashi Ohtsuka <takatronix@gmail.com>
2017-03-06 10:17:43 +0900

    ファイル名を修正

--- src/main/java/red/man10/Man10Kit.java
+++ src/main/java/red/man10/Man10Kit.java
@@ -2,7 +2,7 @@ package red.man10;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class kit extends JavaPlugin {
+public final class Man10Kit extends JavaPlugin {
 
     @Override
     public void onEnable() {
